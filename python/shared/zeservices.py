d60e834264d0e42cfb7b2d94ef6b1d7c96632bac
epenet <epenet@infinityccs.com>
2019-01-07 10:51:22 +0100

    Added support for xsrfToken

--- python/shared/zeservices.py
+++ python/shared/zeservices.py
@@ -54,11 +54,17 @@ class ZEServices:
    if(time.gmtime() > time.gmtime(token['exp'])):
     url = ZEServices.servicesHost + '/api/user/token/refresh'
 
-    # We just send the file as we are not adding anything else into it, if this changes the following 2 key-value pairs are mandatory.
-    #payload = {'refresh_token': tokenData['refresh_token'], 'token': tokenData['token']}
-    payload = tokenData
+    # refreshToken is now sent using cookies,
+    # and xsrfToken is sent using headers
+    # so only the token is required in the payload
+    payload = {'token': tokenData['token']}
 
-    response = requests.post(url, headers=ZEServices.stealthyHeaders, json=payload).json()
+    headers = ZEServices.stealthyHeaders
+    headers['X-XSRF-TOKEN'] = tokenData['xsrfToken']
+    cookies = {'refreshToken': tokenData['refreshToken']}
+
+    ses = requests.Session()
+    response = ses.post(url, headers=headers, cookies=cookies, json=payload).json()
 
     # Overwrite the current token with this newly returned one.
     tokenData['token'] = response['token']
@@ -74,10 +80,11 @@ class ZEServices:
   except FileNotFoundError:
    url = ZEServices.servicesHost + '/api/user/login'
    payload = {'username':user, 'password':password}
-   api_json = requests.post(url, headers=ZEServices.stealthyHeaders, json=payload).json()
+   ses = requests.Session()
+   api_json = ses.post(url, headers=ZEServices.stealthyHeaders, json=payload).json()
 
    # We do not want to save all the user data returned on login, so we create a smaller file of just the mandatory information.
-   tokenData = {'refresh_token' : api_json['refresh_token'], 'token' : api_json['token']}
+   tokenData = {'refreshToken' : ses.cookies['refreshToken'], 'xsrfToken' : api_json['xsrfToken'], 'token' : api_json['token']}
 
    # Save this refresh token and JWT token for future use so we are nicer to Renault's authentication server.
    with open('credentials_token.json', 'w') as outfile:
